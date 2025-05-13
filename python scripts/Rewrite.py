e71ad73299110ea1d31e822b09462088e1ba8e75
Lokesh <lokesh59484@gmail.com>
2025-05-14 01:14:11 +0530

    updates

--- python scripts/Rewrite.py
+++ python scripts/Rewrite.py
@@ -1,19 +1,29 @@
 import os
 from pprint import *
+import numpy as np
 dir = '/home/loki/Research/Ethyl_Propiolate'
-ext = ('.png','.kinp','.kstp')
+ext = ('.gjf')
 i=0
 prefixes = [
-'ts11',
-'ts6',
-'ts7',
-'ts8',
-'ts2',
-'ts4'
+'ts11a',
+'ts6a',
+'ts7a',
+'ts8a',
+'ts2a',
+'ts4a',
+'ts1a' #to be renamed after all processes are completed
 ]
-print('started')
+ind1 = np.arange(1,22)
+ind2 = np.flip(ind1)
+match = {str(ind1[i]).zfill(2):str(ind2[i]).zfill(2) for i in range(len(ind1))}
 os.system("clear")
 for files in os.walk(dir, topdown = True):
     for file in files[2]:
-        if '_irc_points' in files[0] and not any(x in files[0] for x in prefixes):
-            print(file)
+        if  file.endswith(ext) and '_irc_points' in files[0] and not any(x in files[0] for x in prefixes):
+            point = file[file.index('nt')+2:file.index('nt')+4]
+            newname = file.replace(point,match[point])
+#            os.rename(files[0]+'/'+file,files[0]+'/'+newname)
+            i+=1
+            print(file,"<---->",newname)
+            break
+print(i)
