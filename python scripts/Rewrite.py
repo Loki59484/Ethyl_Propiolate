20b484988970e2e170044876f2349fc1a66a93ee
Lokesh <lokesh59484@gmail.com>
2025-05-14 01:48:44 +0530

    updates

--- python scripts/Rewrite.py
+++ python scripts/Rewrite.py
@@ -2,8 +2,9 @@ import os
 from pprint import *
 import numpy as np
 dir = '/home/loki/Research/Ethyl_Propiolate'
-ext = ('.gjf')
+ext = ('.log')
 i=0
+os.system("clear")
 prefixes = [
 'ts11a',
 'ts6a',
@@ -16,14 +17,12 @@ prefixes = [
 ind1 = np.arange(1,22)
 ind2 = np.flip(ind1)
 match = {str(ind1[i]).zfill(2):str(ind2[i]).zfill(2) for i in range(len(ind1))}
-os.system("clear")
 for files in os.walk(dir, topdown = True):
     for file in files[2]:
-        if  file.endswith(ext) and '_irc_points' in files[0] and not any(x in files[0] for x in prefixes):
-            point = file[file.index('nt')+2:file.index('nt')+4]
-            newname = file.replace(point,match[point])
-#            os.rename(files[0]+'/'+file,files[0]+'/'+newname)
-            i+=1
+        if  file.endswith(ext) and '_irc_points' in files[0] and not any(x in files[0] for x in prefixes) and not 'ccsdt' in file:
+            point = file[file.index('NT')+2:file.index('NT')+4]
+            newname = file.replace(point,match[point]).replace('.','new.')
+            os.rename(files[0]+'/'+file,files[0]+'/'+newname)
             print(file,"<---->",newname)
-            break
+            i+=1
 print(i)
