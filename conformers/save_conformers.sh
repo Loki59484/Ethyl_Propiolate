0087e4a4535fa4f2d1642465656a078e2f68c6b7
Lokesh <lokesh59484@gmail.com>
2025-06-01 11:43:02 +0530

    updates

--- conformers/save_conformers.sh
+++ conformers/save_conformers.sh
@@ -10,13 +10,14 @@ import os
 os.system("clear")
 for files in os.walk("/home/loki/Research/Ethyl_Propiolate/conformers", topdown=True):
 	for file in files[2]:
-		source = open(files[0]+"/"+file,'r')
-		temp = source.readlines()
-		temp.remove(temp[0])
-		temp.remove(temp[0])
-		for item in reversed(["%nprocshared=24\n","%mem=12GB\n","%chk=ethyl_propiolate.chk\n","# opt=(calcfc,tight,noeigentest) freq 6-31+g m062x\n"]):
-			temp.insert(0,item)
-		with open(files[0]+"/"+file,'w') as output:
-			output.writelines(temp)
+		if file.endswith(".gjf"):
+			source = open(files[0]+"/"+file,'r')
+			temp = source.readlines()
+			temp.remove(temp[0])
+			temp.remove(temp[0])
+			for item in reversed(["%nprocshared=24\n","%mem=12GB\n","%chk=ethyl_propiolate.chk\n","# opt=(calcfc,tight,noeigentest) freq 6-31+g m062x\n"]):
+				temp.insert(0,item)
+			with open(files[0]+"/"+file,'w') as output:
+				output.writelines(temp)
 exit()
 
