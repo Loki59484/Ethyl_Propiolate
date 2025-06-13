8c48038d2ac01f8545379c9dd351915d18612a99
Loki <lokesh59484@gmail.com>
2025-06-13 17:34:15 +0530

    ccsdt ran

--- Ethyl_prop_non-planar/run_ccsdt.sh
+++ Ethyl_prop_non-planar/run_ccsdt.sh
@@ -3,8 +3,7 @@
 trap "echo 'Interrupted by Ctrl+C. Exiting loop.'; exit 1" SIGINT;
 ''>"completed_queue.txt";
 find -name "*irc.gjf" | while read file; do                                                                                              
-echo $file
- >> "completed_queue.txt"
+echo $file >> "completed_queue.txt"
 g16 < "$file" | tee "${file/.gjf/.log}"
     if [ $? -ne 0 ]; then
         echo "Error running g16 on $file. Exiting loop."
