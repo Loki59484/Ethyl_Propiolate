f78ba34d62f43d1e1766899e1f2156aaca9e994c
Loki <lokesh59484@gmail.com>
2025-07-05 13:43:42 +0530

    ccsdt

--- Ethyl_prop_non-planar/run_ccsdt.sh
+++ Ethyl_prop_non-planar/run_ccsdt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 trap "echo 'Interrupted by Ctrl+C. Exiting loop.'; exit 1" SIGINT;
 find -name "*ircpoint*.gjf" | while read file; do                                                                                              
-if grep -Fxq $file "completed_queue.txt";then
+if grep -Fxq "${file/.gjf/ccsdt.log}" "completed_queue.txt";then
 	continue
-fi
-echo $file >> "completed_queue.txt"
+fi 
+echo "${file/.gjf/ccsdt.log}" >> "completed_queue.txt"
 g16 < "$file" | tee "${file/.gjf/ccsdt.log}"
     if [ $? -ne 0 ]; then
         echo "Error running g16 on $file. Exiting loop."
