10efbd3791f93567a849baf0d2afda27e0925f54
Lokesh_college <lokesh59484@gmail.com>
2025-07-18 15:32:20 +0530

    ccsdt ran

---
+++ Ethyl_prop_non-planar/run_ccsdt_college.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+trap "echo 'Interrupted by Ctrl+C. Exiting loop.'; exit 1" SIGINT;
+find -name "*ircpoint*.gjf" | while read file; do
+if grep -Fxq "${file/.gjf/ccsdt.log}" "completed_queue.txt";then
+        continue
+fi 
+
+if  [[ "$file" == *"Addition/ts1add_ircpoints"* ]];then
+echo "${file/.gjf/ccsdt.log}" >> "completed_queue_college.txt"
+g16 < "$file" | tee "${file/.gjf/ccsdt.log}"
+    if [ $? -ne 0 ]; then
+        echo "Error running g16 on $file. Exiting loop."
+        break
+    fi
+git add .;git commit -m 'ccsdt ran';git push origin main
+find ~/ -type f -name "*.rwf" -mtime +1 -delete
+fi;
+done
+
+
+
