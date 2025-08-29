a05e6cf1bb0a59d211262684fddce69ce1cbe437
Loki <lokesh59484@gmail.com>
2025-08-29 15:22:33 +0530

    temp

--- Ethyl_prop_non-planar/run_ccsdt.sh
+++ Ethyl_prop_non-planar/run_ccsdt.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+asusctl profile -P Performance
 trap "echo 'Interrupted by Ctrl+C. Exiting loop.'; exit 1" SIGINT;
 find -name "*irc*ccsdt.gjf" |sort | while read file; do                                                                                              
 
-if grep -Fxq "${file/.gjf/ccsdt.log}" "completed_queue.txt";then
+if grep -Fxq "${file/.gjf/.log}" "completed_queue.txt";then
 	continue
 fi 
 
-if grep -Fxq "${file/.gjf/ccsdt.log}" "completed_queue_college.txt";then
+if grep -Fxq "${file/.gjf/.log}" "completed_queue_college.txt";then
 	continue
 fi
 
-echo "${file/.gjf/ccsdt.log}" >> "completed_queue.txt"
-g16 < "$file" | tee "${file/.gjf/ccsdt.log}"
+echo "${file/.gjf/.log}" >> "completed_queue.txt"
+g16 < "$file" | tee "${file/.gjf/.log}"
     if [ $? -ne 0 ]; then
         echo "Error running g16 on $file. Exiting loop."
+        asusctl profile -P Quiet
+        find ~/ -type f -name "*.rwf" -mtime +1 -delete
         break
     fi                                                                                                                                                             
- done
+echo "Process Completed. Sleeping..."
+sleep 3m
+done
 git add .;git commit -m 'ccsdt ran';git push origin main
 find ~/ -type f -name "*.rwf" -mtime +1 -delete
+asusctl profile -P Performance
+
